b9fbefcc2a38319c965e4aafa8c3f47295381a7d
Guilherme Barroso <barroso.guilherme@gmail.com>
2017-08-17 19:54:33 +0000

    funcionando Directions Service

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -33.8688, lng: 151.2195},
-    zoom: 13
+    center: {lat: -23.5337, lng: -46.6252},
+    zoom: 12
   });
 
   var input = document.getElementById('pac-input');
