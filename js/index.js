0b3f6b410b2764061941f738e973be2392f9ca4d
Guilherme Barroso <barroso.guilherme@gmail.com>
2017-08-18 19:54:57 +0000

    última atualização

--- js/index.js
+++ js/index.js
@@ -1,5 +1,88 @@
 function initMap() {
 
+  // Tentando mudar o estilo do mapa
+  // var styledMapType = new google.maps.StyledMapType( [
+  //   {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+  //   {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+  //   {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+  //   {
+  //     featureType: 'administrative.locality',
+  //     elementType: 'labels.text.fill',
+  //     stylers: [{color: '#d59563'}]
+  //   },
+  //   {
+  //     featureType: 'poi',
+  //     elementType: 'labels.text.fill',
+  //     stylers: [{color: '#d59563'}]
+  //   },
+  //   {
+  //     featureType: 'poi.park',
+  //     elementType: 'geometry',
+  //     stylers: [{color: '#263c3f'}]
+  //   },
+  //   {
+  //     featureType: 'poi.park',
+  //     elementType: 'labels.text.fill',
+  //     stylers: [{color: '#6b9a76'}]
+  //   },
+  //   {
+  //     featureType: 'road',
+  //     elementType: 'geometry',
+  //     stylers: [{color: '#38414e'}]
+  //   },
+  //   {
+  //     featureType: 'road',
+  //     elementType: 'geometry.stroke',
+  //     stylers: [{color: '#212a37'}]
+  //   },
+  //   {
+  //     featureType: 'road',
+  //     elementType: 'labels.text.fill',
+  //     stylers: [{color: '#9ca5b3'}]
+  //   },
+  //   {
+  //     featureType: 'road.highway',
+  //     elementType: 'geometry',
+  //     stylers: [{color: '#746855'}]
+  //   },
+  //   {
+  //     featureType: 'road.highway',
+  //     elementType: 'geometry.stroke',
+  //     stylers: [{color: '#1f2835'}]
+  //   },
+  //   {
+  //     featureType: 'road.highway',
+  //     elementType: 'labels.text.fill',
+  //     stylers: [{color: '#f3d19c'}]
+  //   },
+  //   {
+  //     featureType: 'transit',
+  //     elementType: 'geometry',
+  //     stylers: [{color: '#2f3948'}]
+  //   },
+  //   {
+  //     featureType: 'transit.station',
+  //     elementType: 'labels.text.fill',
+  //     stylers: [{color: '#d59563'}]
+  //   },
+  //   {
+  //     featureType: 'water',
+  //     elementType: 'geometry',
+  //     stylers: [{color: '#17263c'}]
+  //   },
+  //   {
+  //     featureType: 'water',
+  //     elementType: 'labels.text.fill',
+  //     stylers: [{color: '#515c6d'}]
+  //   },
+  //   {
+  //     featureType: 'water',
+  //     elementType: 'labels.text.stroke',
+  //     stylers: [{color: '#17263c'}]
+  //   }
+  // ]
+  // {name: 'Night Map'});
+
   // definindo configurações do meu mapa
   var mapOptions = {
     mapTypeControl: true,
@@ -29,26 +112,35 @@ function AutocompleteDirectionsHandler(map) {
 
   // Pedindo permissão ao usuário - Geolocation
   var infoWindow = new google.maps.InfoWindow({map: map});
-  // Try HTML5 geolocation.
+
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
+
       var pos = {
         lat: position.coords.latitude,
         lng: position.coords.longitude
       };
 
-      infoWindow.setPosition(pos);
-      infoWindow.setContent('Location found.');
+      var marker = new google.maps.Marker({
+        position: pos,
+        map: map,
+        title: 'Você está aqui'
+      });
+
+      // dubstituindo o infowindow pelo marker
+
+      // infoWindow.setPosition(pos);
+      // infoWindow.setContent('Você está aqui');
       map.setCenter(pos);
-    }, function() {
-      handleLocationError(true, infoWindow, map.getCenter());
+    },
+    function() {
+      handleLocationError(true, marker, map.getCenter());
     });
   } else {
-    // Browser doesn't support Geolocation
-    handleLocationError(false, infoWindow, map.getCenter());
+    handleLocationError(false, marker, map.getCenter());
   }
 
-  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  function handleLocationError(browserHasGeolocation, marker, pos) {
     infoWindow.setPosition(pos);
     infoWindow.setContent(browserHasGeolocation ?
       'Error: The Geolocation service failed.' :
@@ -80,8 +172,9 @@ function AutocompleteDirectionsHandler(map) {
 
         // Rearrumei a ordem para posicionar os inputs e selectores
         this.map.controls[google.maps.ControlPosition.LEFT_BOTTOM].push(modeSelector);
-        this.map.controls[google.maps.ControlPosition.LEFT_BOTTOM].push(originInput);
         this.map.controls[google.maps.ControlPosition.LEFT_BOTTOM].push(destinationInput);
+
+        this.map.controls[google.maps.ControlPosition.LEFT_BOTTOM].push(originInput);
       }
 
       // Sets a listener on a radio button to change the filter type on Places
